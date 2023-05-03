8c59f6caad2f566cd08204bda17c0c71815f9fc0
ronymolkanen <111876594+ronymolkanen@users.noreply.github.com>
2023-05-03 10:27:16 +0300

    Update Testing.java

--- WEB-SERVICES/Testing.java
+++ WEB-SERVICES/Testing.java
@@ -1,18 +1,15 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import data.Robot;
@@ -21,13 +18,6 @@ import data.Robot;
 public class Testing {
 
 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("robot");
-
-	@GET
-	@Path("/sayhello")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String Hello() {
-		return "wazaaa";
-	}
 
 	@POST
 	@Path("/newrobotvalues")
@@ -39,12 +29,12 @@ public class Testing {
 		em.persist(robot);
 		em.getTransaction().commit();
 		return robot;
-
 	}
+	
 	@GET
 	@Path("/getlatestvalues")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Robot> getLatestStats() {
+	public Robot getLatestStats() {
 		EntityManager em = emf.createEntityManager();
 		int maxId = (int) em.createQuery("SELECT MAX(R.id) FROM Robot R").getSingleResult();
 		int lastId = maxId;
@@ -54,8 +44,25 @@ public class Testing {
 
 		em.close();
 		emf.close();
-		return list;
+		return list.get(0);
+	}
+	
+	@GET
+	@Path("/newrobovalues")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String newRoboValues() {
+		EntityManager em = emf.createEntityManager();
+		int maxId = (int) em.createQuery("SELECT MAX(R.id) FROM Robot R").getSingleResult();
+		int lastId = maxId;
 
+		List<Robot> list = em.createQuery("SELECT R FROM Robot R WHERE R.id >= :lastId").setParameter("lastId", lastId)
+				.getResultList();
+
+		em.close();
+		emf.close();
+		 Robot r=list.get(0);
+	     String palaute=""+r.getAcceleration()+" "+r.getSpeedmulti()+ " "+ r.getBlack_value()+ " "+ r.getWhite_value();
+	     return palaute;
 	}
 
 	@GET
@@ -68,6 +75,12 @@ public class Testing {
 		em.close();
 		emf.close();
 		return list;
-
+	}
+	
+	@GET
+	@Path("/speedfromrobot")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getSpeed() {
+		return null;
 	}
 }
