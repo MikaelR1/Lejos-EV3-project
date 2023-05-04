c23ab415697dab73ff481c549cffb34ad5dc2e2b
ronymolkanen <111876594+ronymolkanen@users.noreply.github.com>
2023-05-04 11:15:05 +0300

    Update Testing.java

--- WEB-SERVICES/Testing.java
+++ WEB-SERVICES/Testing.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import data.Robot;
+import data.Speeddata;
 
 @Path("/testing")
 public class Testing {
@@ -48,6 +52,22 @@ public class Testing {
 	}
 	
 	@GET
+	@Path("/getdbspeed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Speeddata getDbSpeed() {
+		EntityManager em = emf.createEntityManager();
+		int maxId = (int) em.createQuery("SELECT MAX(R.id) FROM Speeddata R").getSingleResult();
+		int lastId = maxId;
+
+		List<Speeddata> list = em.createQuery("SELECT R FROM Speeddata R WHERE R.id >= :lastId").setParameter("lastId", lastId)
+				.getResultList();
+
+		em.close();
+		emf.close();
+		return list.get(0);
+	}
+	
+	@GET
 	@Path("/newrobovalues")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String newRoboValues() {
@@ -64,23 +84,37 @@ public class Testing {
 	     String palaute=""+r.getAcceleration()+" "+r.getSpeedmulti()+ " "+ r.getBlack_value()+ " "+ r.getWhite_value();
 	     return palaute;
 	}
-
-	@GET
-	@Path("/getallvalues")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Robot> getAllStats() {
-		EntityManager em = emf.createEntityManager();
-		List<Robot> list = em.createQuery("SELECT R FROM Robot R").getResultList();
-
-		em.close();
-		emf.close();
-		return list;
-	}
 	
 	@GET
-	@Path("/speedfromrobot")
+	@Path("/speedfromrobot/{lcurrent_speed}/{rcurrent_speed}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getSpeed() {
-		return null;
+	public Response speedFromRobot(@PathParam("lcurrent_speed") float lcurrentspeed,
+	                               @PathParam("rcurrent_speed") float rcurrentspeed) {
+		
+	    try {
+	        // Create a new connection to the database
+	        EntityManager em = emf.createEntityManager();
+	        em.getTransaction().begin();
+
+	        // Create a new instance of the SpeedData entity class
+	        Speeddata speedData = new Speeddata();
+	        speedData.setLcurrent_speed(lcurrentspeed);
+	        speedData.setRcurrent_speed(rcurrentspeed);
+
+	        // Persist the SpeedData object in the database
+	        em.persist(speedData);
+
+	        // Commit the transaction and close the entity manager
+	        em.getTransaction().commit();
+	        em.close();
+
+	        // Return a successful response
+	        return Response.ok().build();
+	    } catch (Exception e) {
+	        // Return an error response with the error message
+	        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+	                .entity("Failed to save data: " + e.getMessage())
+	                .build();
+	    }
 	}
 }
