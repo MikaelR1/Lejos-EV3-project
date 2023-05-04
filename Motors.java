0265a6869f5443adde8f59fdb9f91c0ee9ac6bb6
ronymolkanen <111876594+ronymolkanen@users.noreply.github.com>
2023-05-04 11:13:00 +0300

    Update Motors.java

--- Motors.java
+++ Motors.java
@@ -1,75 +1,108 @@
+import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.utility.Delay;
 
 public class Motors extends Thread {
-// this is new comment 
-    private DataExchange DE;
-    private EV3ColorSensor cs;
-    private EV3LargeRegulatedMotor leftMotor;
-    private EV3LargeRegulatedMotor rightMotor;
-    private final int ACCELERATION = 1000;
 
-    public Motors(DataExchange dataExchange) {
-        this.DE = dataExchange;
-        leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-        rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
-    }
+	private DataExchange DE;
+	private EV3LargeRegulatedMotor leftMotor;
+	private EV3LargeRegulatedMotor rightMotor;
 
-    public Motors(EV3ColorSensor cs) {
-        this.cs = cs;
-        leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-        rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
-    }
+	public Motors(DataExchange dataExchange) {
+		this.DE = dataExchange;
+		leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+		rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+	}
 
-    @Override
-    public void run() {
-        //Comment here 
-        while (true) {
+	@Override
 
-            switch (DE.getCommand()) {
+	// Override the run() method of the Thread class
+	public void run() {
 
-                case DataExchange.FOLLOW_LINE: // Follow line
+		// Acceleration for the motors
+		leftMotor.setAcceleration(DE.getAcceleration());
+		rightMotor.setAcceleration(DE.getAcceleration());
 
-                    leftMotor.setAcceleration(ACCELERATION);
-                    rightMotor.setAcceleration(ACCELERATION);
+		while (true) {
+			
+			if (DE.getCommand() == DataExchange.FOLLOW_LINE) { // Follow line command
 
-                    while (DE.getCommand() == DataExchange.FOLLOW_LINE) {
-                    	System.out.println(DE.getDeviation());
-                        leftMotor.setSpeed(200 + (int) (DE.getDeviation() * 500));
-                        rightMotor.setSpeed(200 - (int) (DE.getDeviation() * 500));
-                        leftMotor.forward();
-                        rightMotor.forward();
-                    }
-                    break;
+				// Set the speed of the motors based on deviation
+				leftMotor.setSpeed(200 + (int) (DE.getDeviation() * DE.getSpeedmulti()));
+				rightMotor.setSpeed(200 - (int) (DE.getDeviation() * DE.getSpeedmulti()));
 
-                case DataExchange.STOP: // Stop
+				System.out.println("Vasen moottori: " + leftMotor.getSpeed());
+				System.out.println("Oikea moottori: " + rightMotor.getSpeed());
+				
+				float lcurrent_speed = leftMotor.getSpeed();
+				float rcurrent_speed = rightMotor.getSpeed();
+				DE.setLcurrent_speed(lcurrent_speed);
+				DE.setRcurrent_speed(rcurrent_speed);
+				
+				// Set the motors go forward
+				leftMotor.forward();
+				rightMotor.forward();
 
-                    leftMotor.stop();
-                    rightMotor.stop();
-                    break;
+			} else if (DE.getCommand() == DataExchange.END) { // End routine command
 
-                case DataExchange.AVOID: // Avoid obstacle
+				// Stop the motors
+				leftMotor.stop();
+				rightMotor.stop();
 
-                    DE.setObstacleDetected(false); // Reset obstacle detection flag
-                    rightMotor.rotate(180);
-                    rightMotor.stop();
-                    leftMotor.setSpeed(100);
-                    rightMotor.setSpeed(50);
-                    leftMotor.forward();
-                    rightMotor.forward();
+				// Make the robot spin...
+				leftMotor.setSpeed(600);
+				rightMotor.setSpeed(600);
+				leftMotor.backward();
+				rightMotor.forward();
 
-                    if(DE.getDeviation() <= 0.20)
-                    	DE.setCommand(1);
-                    
-                    while (!DE.getObstacleDetected()) {
-                        // Keep rotating until obstacle is detected
-                    }
+				// ...and play some sounds
+				Sound.playTone(400, 1000);
+				Sound.playTone(600, 1500);
+				Sound.playTone(800, 1500);
+				Sound.playTone(1000, 1000);
 
-                    leftMotor.stop();
-                    rightMotor.stop(); // Stop after avoiding obstacle
-                    break;
-            }
-        }
-    }
+				// Make the ending show last 5sec
+				Delay.msDelay(5000);
+
+				// Finally, stop the motors and break the loop
+				leftMotor.stop();
+				rightMotor.stop();
+				break;
+
+			} else if (DE.getCommand() == DataExchange.AVOID) { // Avoid obstacle command
+
+				// Move robot left for 1,8sec
+				leftMotor.setSpeed(150);
+				rightMotor.setSpeed(300);
+				leftMotor.forward();
+				rightMotor.forward();
+
+				Delay.msDelay(1800);
+
+				// Move robot right for 2sec
+				leftMotor.setSpeed(300);
+				rightMotor.setSpeed(100);
+				leftMotor.forward();
+				rightMotor.forward();
+
+				Delay.msDelay(2000);
+
+				// Move robot straight 1,4sec
+				leftMotor.setSpeed(200);
+				rightMotor.setSpeed(200);
+				leftMotor.forward();
+				rightMotor.forward();
+
+				Delay.msDelay(1400);
+
+				// Rotate the robot so it is easier for it to continue to follow a line
+				leftMotor.rotate(90);
+				Delay.msDelay(200);
+
+				// Robot continues to follow a line
+				DE.setCommand(DataExchange.FOLLOW_LINE);
+			}
+		}
+	}
 }
