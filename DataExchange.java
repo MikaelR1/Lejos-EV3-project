f41c60e39bfa94ceb4ed4b28b414451a24f6e355
ronymolkanen <111876594+ronymolkanen@users.noreply.github.com>
2023-05-04 11:13:29 +0300

    Update DataExchange.java

--- DataExchange.java
+++ DataExchange.java
@@ -3,9 +3,24 @@
  */
 public class DataExchange {
 
+	// ObstacleDetector
+	private volatile boolean obstacleDetected = false;
+	// Avoid obstacle
+	private volatile boolean avoidObstacle = false;
+
+	// Robot follow commands
+	public final static int FOLLOW_LINE = 1;
+	public final static int END = 2;
+	public final static int AVOID = 3;
 
 	private int command = FOLLOW_LINE; // The current robot command
 	private float deviation = 0; // The deviation of the robot from the line
+	private int speedmulti = 0;
+	private int acceleration = 0;
+	private float black_value = 0;
+	private float white_value = 0;
+	private float rcurrent_speed = 0;    
+	private float lcurrent_speed = 0;
 
 	/**
 	 * Sets the obstacle detection status.
@@ -77,5 +92,53 @@ public class DataExchange {
 	 */
 	public void setDeviation(float deviation) {
 		this.deviation = deviation;
+	}
+
+	public int getSpeedmulti() {
+		return speedmulti;
+	}
+
+	public void setSpeedmulti(int speedmulti) {
+		this.speedmulti = speedmulti;
+	}
+
+	public int getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(int acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public float getBlack_value() {
+		return black_value;
+	}
+
+	public void setBlack_value(float black_value) {
+		this.black_value = black_value;
+	}
+
+	public float getWhite_value() {
+		return white_value;
+	}
+
+	public void setWhite_value(float white_value) {
+		this.white_value = white_value;
+	}
+
+	public float getRcurrent_speed() {
+		return rcurrent_speed;
+	}
+
+	public void setRcurrent_speed(float rcurrent_speed) {
+		this.rcurrent_speed = rcurrent_speed;
+	}
+
+	public float getLcurrent_speed() {
+		return lcurrent_speed;
+	}
+
+	public void setLcurrent_speed(float lcurrent_speed) {
+		this.lcurrent_speed = lcurrent_speed;
 	}
 }
