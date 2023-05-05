f9e88b3a8017f6bac7262a8e641a0f7525c38773
ronymolkanen <111876594+ronymolkanen@users.noreply.github.com>
2023-05-05 12:06:13 +0300

    Update Robot.java

--- WEB-SERVICES/Robot.java
+++ WEB-SERVICES/Robot.java
@@ -7,27 +7,29 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="lego_control_panel")
+@Entity // This annotation indicates that this class is an entity that can be persisted to database
+		
+@Table(name = "lego_control_panel") // specifies the name of the database table
 public class Robot {
 
-	@Id
+	@Id // indicates that this field is the primary key of this entity
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Column (name="speedmulti")
+	@Column(name = "speedmulti") // This annotation specifies the name of the column in the database table
 	private int speedmulti;
-	
-	@Column (name="acceleration")
+
+	@Column(name = "acceleration") // This annotation specifies the name of the column in the database table
 	private int acceleration;
-	
-	@Column (name="black_value")
+
+	@Column(name = "black_value") // This annotation specifies the name of the column in the database table
 	private float black_value;
-	
-	@Column (name="white_value")
+
+	@Column(name = "white_value") // This annotation specifies the name of the column in the database table
 	private float white_value;
 
-public int getId() {
+	// Getters and setters
+	public int getId() {
 		return id;
 	}
 
@@ -67,24 +69,22 @@ public int getId() {
 		this.white_value = white_value;
 	}
 
-public Robot(int speedmulti, int acceleration, float black_value, float white_value) {
-    this.speedmulti = speedmulti;
-    this.acceleration = acceleration;
-    this.black_value = black_value;
-    this.white_value = white_value;
-}
-public Robot() {
-	//Default constructor ?
-}
-@Override
-public String toString() {
-    return "Robot{" +
-            "id=" + id +
-            ", speed=" + speedmulti +
-            ", acceleration=" + acceleration +
-            ", blackValue=" + black_value +
-            ", whiteValue=" + white_value +
-            '}';
+	// Constructors for the class
+	public Robot(int speedmulti, int acceleration, float black_value, float white_value) {
+		this.speedmulti = speedmulti;
+		this.acceleration = acceleration;
+		this.black_value = black_value;
+		this.white_value = white_value;
+	}
 
-}
+	public Robot() {
+		// Default constructor
+	}
+	
+	//toString() method to return a string representation of the object
+	@Override
+	public String toString() {
+		return "Robot{" + "id=" + id + ", speed=" + speedmulti + ", acceleration=" + acceleration + ", blackValue="
+				+ black_value + ", whiteValue=" + white_value + '}';
+	}
 }
